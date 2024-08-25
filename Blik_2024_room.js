695a8eb5a290fc04d2a0fe78ddcb78267073edef
blik <blik@interface.org>
2024-08-26 01:51:01 +0200

    parse inference in text, fix persistence
    split d3 sources

--- Blik_2024_room.js
+++ Blik_2024_room.js
@@ -3,86 +3,88 @@
  import {fetch,resolve} from "./Blik_2023_interface.js";
  import {merge} from "./Blik_2023_search.js";
 
- export async function open(server,room="./Blik_2020_room.js")
+ export async function open(server,actions)
 {let {default:{WebSocketServer}}=await import("./einaros_2011_ws.js");
  let connection=buffer(async function connection(host,peer,request)
 {let {referer,cookie}=request.headers;
- let author=cookie&&cookie.match(/signature=[^;]+/);
- let url=author&&author[0].replace("=","/");
- ({author}=url?await fetch(url):{author:"anonymous"});
+ let signature=cookie&&cookie.match(/signature=[^;]+/);
+ let url=signature?.[0].replace("=","/");
+ let {author}=url?await fetch(url):{author:{name:"anonymous"}};
  observe.call(Object.assign(peer,{author}),{message:infer(transmit,host)});
 },note.bind(1));
- return compose([{server}],Reflect.construct,{rooms:{}},Object.assign,{connection},tether(observe))(WebSocketServer);
+ return compose([{server}],Reflect.construct,{subjects:{}},Object.assign,{connection},tether(observe))(WebSocketServer);
 };
 
  export var transmit=either
 (function(peer,host,event)
 {let message=JSON.parse(event.data);
- respond[message.type||"message"]?.call(host,message,peer);
+ console.log(message.subject,{[event.type]:message.action});
+ buffer(tether(actions[message.action||"message"]),note.bind(2))(host,message,peer);
  return true;
 },compose(drop(-1,1),(peer,error)=>
  peer.send(JSON.stringify({error:note.call(1,error).message})))
 );
 
- var respond=
- {join({room},peer)
-{this.rooms[room]=this.rooms[room]||{messages:[]};
- let {author}=merge(peer,{rooms:new Set([room])},0);
- let message=JSON.stringify({author:system,message:author+" joined "+room,room});
- this.clients.forEach(client=>client!==peer&&client.rooms.has(room)&&client.send(message));
- let {length}=this.rooms[room].messages;
+ var actions=
+ {join({subject},peer)
+{this.subjects[subject]=this.subjects[subject]||{messages:[]};
+ let {author}=merge(peer,{subjects:new Set([subject])},0);
+ let message=JSON.stringify({author:system,message:author+" joined "+subject,subject});
+ this.clients.forEach(client=>client!==peer&&client.subjects.has(subject)&&client.send(message));
+ let {length}=this.subjects[subject].messages;
  if(length)
- peer.send(JSON.stringify({type:"history",history:this.rooms[room].messages}));
+ peer.send(JSON.stringify({type:"history",history:this.subjects[subject].messages}));
+ return this.subjects[subject];
 },sign({author},peer){peer.author=author;}
- ,signal({room},peer)
-{let response=JSON.stringify({type:"signal",author:peer.author});
- this.clients.forEach(client=>client.readyState===1&&
- client.send(response));
+ ,signal({subject},peer)
+{let event={type:"signal",author:peer.author};
+ actions.broadcast.call(this,event);
 },message({message},{author})
-{let response=JSON.stringify({author,message});
+{let event={author,message};
+ actions.broadcast.call(this,event);
+},broadcast(event)
+{let message=JSON.stringify(event);
  this.clients.forEach(client=>client.readyState===1&&
- client.send(response));
-},save:async function({room,updates,version})
-{this.room=this.adapter.rooms[room]||this.join(room);
- let {EditorState,collab,receiveUpdates,getSyncedVersion,ChangeSet}=
- await import("./haverbeke_2020_codemirror.js");
- if(!this.room.track)this.room.track=await fetch(room).then(doc=>
+ client.send(message));
+},save:async function({subject,content,updates,version},peer)
+{peer.subject=this.subjects[subject]||actions.join.call(...arguments);
+ let {EditorState,collab,receiveUpdates,getSyncedVersion,ChangeSet}=await import("./haverbeke_2020_codemirror.js");
+ if(!peer.subject.track)
+ peer.subject.track=await fetch(subject).then(doc=>
  EditorState.create({doc:doc.toString(),extensions:[collab()]}));
- //if(note([version,getSyncedVersion(this.room.content)]).reduce((next,past)=>next>past))
- await update(this.room.track,updates);
- let body=this.room.track.doc.toString();
+ //if(note([version,getSyncedVersion(this.subject.content)]).reduce((next,past)=>next>past))
+ await update(peer.subject.track,updates);
+ let body=this.subject.track.doc.toString();
  let headers={"Content-Type":"text/plain"};
- let message=await fetch(room+"?force=overwrite",{method:"put",body,headers});
+ let message=await fetch(subject+"?force=overwrite",{method:"put",body,headers});
  if(message instanceof Error)
  return this.emit("message",{author:system,message});
- else message=this.author+" edited "+room;
-[["save",{author:this.author,room,updates}]
+ else message=this.author+" edited "+subject;
+[["save",{author:this.author,subject,updates}]
 ,["message",{author:system,message}]
 ].map(event=>
- this.server.sockets.in(room).emit(...event));
+ this.server.sockets.in(subject).emit(...event));
 },disconnect(text)
-{Object.keys(this.adapter.rooms).forEach(room=>
- this.leave(room)&&
- this._events.message.bind(this)({author:system,message:this.author+" left "+room,room}));
-},put:async function({room,body})
-{this.room=this.adapter.rooms[room]||this.join(room);
- if(!this.room.content)
- await fetch(room).then(content=>this.room.content=content);
- this.room.content=JSON.stringify(body);
- let message=await fetch(room+"?force=overwrite"
-,{method:"put",body:this.room.content
- ,headers:{"Content-Type":"text/plain"}
- });
- if(message instanceof Error)
- return this.emit("message",{author:system,message});
-[["put",{body}]
-,["message",{author:system,message:this.author+" updated "+room}]
-].map(event=>this.server.sockets.in(room).emit(...event))
+{Object.keys(this.subjects).forEach(subject=>
+ this.leave(subject)&&
+ this._events.message.bind(this)({author:system,message:this.author+" left "+subject,subject}));
+},put:async function({subject,content},peer)
+{peer.subject=this.subjects[subject]||actions.join.call(...arguments);
+ peer.subject.content=content;
+ let body=Buffer.from(content,"base64").toString("utf8");
+ let message=await fetch(subject+"?force=overwrite",{method:"put",body,headers:{"Content-Type":"text/plain"}});
+ if(message.status!==200)
+ return peer.send("message",{author:system,message:await message.text()});
+[{action:"put",body:content}
+,{action:"message",author:system,message:this.author+" updated "+subject}
+].forEach(event=>actions.broadcast.call(this,event));
 }};
 
  export var receipt=
  {message({type,message,author:{name,image}})
-{message=document({li:{img:{src:image||"svg/deer/document",height:"12px"},div:{"#text":name||"anonymous"},span:{"#text":message}}});
+{if(!this.message)
+ return;
+ message=document({li:{img:{src:image||"svg/deer/document",height:"12px"},div:{"#text":name||"anonymous"},span:{"#text":message}}});
  note(arguments[0]);
  if(name==="system")
  compose(wait(10000),{style:"opacity:0;transition:all 1s;"},Object.assign,wait(1000),"remove")(message);
@@ -94,11 +96,11 @@
  let entry=document({span:{id:"signal",signal:window.document.createRange().createContextualFragment(author+" is typing...")}});
  list[node?"replaceChild":"appendChild"](entry,node);
  compose(wait(3000),node=>node.parentNode&&node.remove())(entry);
-},save({author,room,updates})
+},save({author,subject,updates})
 {if(author==window.subject.labels.message)
  return window.Tone.Transport.start();
- update(this.room.content.viewState.state,updates).then(updates=>
- this.room.content.update([updates]));
+ update(this.subject.content.viewState.state,updates).then(updates=>
+ this.subject.content.update([updates]));
 }};
 
  var update=(state,updates)=>
