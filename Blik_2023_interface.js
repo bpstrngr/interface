930134e52e215d46d5b5431fbb8e3dd46c4d35cb
blik <blik@interface.org>
2025-09-23 13:45:53 +0200

    log source purges in interface/acquire

--- Blik_2023_interface.js
+++ Blik_2023_interface.js
@@ -304,8 +304,8 @@
 ,pass(parts=>resolve(entry).catch(note))
 ,parts=>compose.call
 (entry,parts,bundle,slip(absolute),true,access,"bundle ready.",note.bind(2)
-).finally(done=>purge(target)&&delete this[target]&&
- note.call(2,"purged sources of "+target+"."))&&
+).finally(done=>purge(target).then(done=>delete this[target]&&
+ note.call(2,"purged sources of "+target+".")))&&
  // not returning bundle promise after source assembly to unblock immediate resolution from source. 
  entry
 )
