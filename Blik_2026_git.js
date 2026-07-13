2f6c9cbdbaa9c77f0e156e770985738c158376c8
blikest <blik@jsrebels.org>
2026-07-13 02:51:27 +0200

    isomorphic-git http module

--- Blik_2026_git.js
+++ Blik_2026_git.js
@@ -1,5 +1,7 @@
- import {record,prune} from "./Blik_2023_inference.js";
+ import {note,buffer,expect,record,prune} from "./Blik_2023_inference.js";
+ import {prompt} from "./Blik_2023_interface.js";
  import {folder} from "./Blik_2023_meta.js";
+ import http from "./Hilton_2018_isomorphic-git-http.js";
  import git from "./Hilton_2017_isomorphic-git.js";
  import fs from "fs";
  var address=import.meta.url;
@@ -24,3 +26,36 @@
 },0,1)
 );
 }};
+
+ export async function check(remote,branch)
+{({remote,branch}=await prompt({remote,branch}));
+ console.log("Pivotting to "+[remote,branch].join("/")+".");
+ let dir=relation;
+ await git.fetch({fs,http,remote,dir}).then(note);
+ await git.checkout(
+ {fs,dir,remote,ref:branch,filepaths:[".gitignore"]
+ ,onProgress(message){console.log(message);}
+ ,onPostCheckout(message){console.log(message);}
+ });
+ await git.add({fs,dir,filepath:"."});
+ //let files=await git.listFiles({fs,dir}).then(note);
+ //let stats=files.map(filepath=>git.status({fs,dir,filepath}).then(note));
+ let status=await git.statusMatrix({fs,dir}).then(note);
+ let {length:stash}=status.filter(([name,head,work,stage])=>work===2).map(([file])=>file);
+ let [name,email]=await Promise.all(["name","email"].map(field=>
+ git.getConfig({fs,path:"user."+field})));
+ await prompt({name,email}).then(config=>Promise.all(
+ Object.entries(config).map(([field,value])=>!{name,email}[field]&&
+ git.setConfig({fs,dir,path:"user."+field,value}))));
+ if(stash)
+ await git.stash({fs,dir,op:"push"}).then(note)
+ await git.stash({fs,dir,op:"list"}).then(note);
+ await git.checkout(
+ {fs,dir,remote,ref:"stash",track:false,noCheckout:true,noUpdateHead:false
+ ,onProgress(message){console.log(message);}
+ ,onPostCheckout(message){console.log(message);}
+ });
+ // git checkout $remote/$branch;
+ // [[ $stash -gt 0 ]] && git checkout stash .;
+ // git restore --staged .; 
+}
